bdcdef8af561338ec597b7260be34aa66092c9d0
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-05-03 11:52:43 -0300

    update

--- Aula08/Exercício05.c
+++ Aula08/Exercício05.c
@@ -1,18 +1,31 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+// Essa função deve imprimir todas as possibilidades de ordem de um vetor:
+void arrayAll(int array[]){
+    
+}
+
 void stairs(int stairsNumber){
-    int matrix[stairsNumber][stairsNumber];
-    for(int i=0; i <stairsNumber; i++){
-        int sum = 0;
-        for(int j=0; j <stairsNumber; j++){
-                     
+    int arraySize = stairsNumber;
+    int *arrayOne;
+    int *arrayTwo;
+    arrayOne = (int*)malloc(arraySize*sizeof(int));
+    arrayTwo = (int*)malloc((arraySize-1)*sizeof(int));
+    for(int i = 0; i < arraySize; i++) arrayOne[i] = 1;
+    int boolEnd = 1;
+    
+
+    while(boolEnd == 1){
+        for(int i = arraySize-1; i > 0; i--){
+
         }
     }
+
     // 1 = [1]
     // 2 = [2] [1, 1]
     // 3 = [2, 1] [1, 1, 1] [1, 2]
-    // 4 = [2, 2] [2, 1, 1]
+    // 4 = [2, 2] [2, 1, 1] 
 }
 
 int main(){
