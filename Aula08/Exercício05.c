24a386c0ff8c50e18ce12307efbd73904ccc96a3
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-05-03 16:25:11 -0300

    update

--- Aula08/Exercício05.c
+++ Aula08/Exercício05.c
@@ -1,79 +1,59 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-void permute(int array[], int size, int n)
-{
-  int tmp;
-  int i, j;
-  if (n == 1) {
-    for (i = 0; i < size; i++)
-      printf("%i ", array[i]);
-    putchar('\n');
-  } 
-  else {
-    for (i = 0; i < n; i++) {
-            permute(array, size, n - 1);
-            j = (n % 2 == 1) ? n - 1 : 0;
-            tmp = array[n - 1];
-            array[n - 1] = array[j];
-            array[j] = tmp;
-        }
-    }
+int factorial(int n){
+    if(n > 1) return n*factorial(n-1);
+    else return 1;
 }
 
-// Essa função deve imprimir todas as possibilidades de ordem de um vetor:
-void arrayAll(int *array, int arraySize, int numberOfTwo){
+int combination(int n, int p){
+    return factorial(n)/(factorial(p)*factorial(n-p));
 }
 
-void stairs(int stairsNumber){
-    
-    // (1) Número máximo de números iguais a 2 no vetor:
+void printArray(int array[], int arraySize){
+    for(int i = 0; i < arraySize; i++) printf("%i ", array[i]);
+    printf("\n");
+}
+
+int stairs(int stairsNumber){
+    // (1) Número de maneiras para se subir a escada:
+    int ways = 0;
+
+    // (2) Número máximo de números iguais a 2 no vetor:
     int result = stairsNumber/2;
-    
-    // (2) Número mínimo de números iguais a 1 no vetor:
+    // Obs.: note que result será sempre igual a quantidade de números iguais à 2 no vetor.
+
+    // (3) Número mínimo de números iguais a 1 no vetor:
     int rest = stairsNumber%2;
+    // Obs.: note que rest será sempre igual a quantidade de números iguais à 1 no vetor.
 
-    // (3) Quando rest == stairsNumber tem-se o vetor [1, 1, 1,...] (apenas com números iguais à 1):
-    while(rest != stairsNumber){
 
-        // (3.1) Vetor de tamanho igual a quantidade de números iguais a 1 somada a quantidade de números iguais a 2:
-        int *array = (int*)malloc((result+rest)*sizeof(int));
+    // (4) Quando rest == stairsNumber tem-se o vetor [1, 1, 1,...] (apenas com números iguais à 1):
+    while(rest <= stairsNumber){
+
+        // (4.1) Vetor de tamanho igual a quantidade de números iguais a 1 somada a quantidade de números iguais a 2:
+        int array[result+rest];
     
-        // (3.2) Adicionando a quantidade de números iguais à 2 no vetor:
+        // (4.2) Adicionando a quantidade de números iguais à 2 no vetor:
         for(int i = 0; i < result; i++) array[i] = 2;
     
-        // (3.3) Adicionando a quantidade de números iguais à 1 no vetor:
-        for(int i = 0; i < rest; i++) array[i+rest] = 1;
+        // (4.3) Adicionando a quantidade de números iguais à 1 no vetor:
+        for(int i = result; i < rest+result; i++) array[i] = 1;
 
-        // (3.4) Chama a função para imprimir todas as permutações do vetor:
-        arrayAll(array, result+rest, result);
-
-        free(array);
-
-        rest += 2;
+        // (4.4) Chama a função para imprimir todas as permutações do vetor:
+        printArray(array, result+rest);
+    
+        // (4.5) Soma o número de permutações possíveis do vetor atual formado por 1's e 2's:
+        ways += combination(result+rest, result);
+        
+        // (4.6) Quando se deixar de ter um número igual à 2 deve-se adicionar dois números iguais à 1:
         result --;
+        rest += 2;
     }
-
-
-    // (3) Vetor de tamanho igual a quantidade de números iguais a 1 somada a quantidade de números iguais a 2:
-    int *array = (int*)malloc((result+rest)*sizeof(int));
-    
-    // (4) Adicionando a quantidade de números iguais à 2 no vetor:
-    for(int i = 0; i < result; i++) array[i] = 2;
-    
-    // (5) Adicionando a quantidade de números iguais à 1 no vetor:
-    for(int i = 0; i < rest; i++) array[i+rest] = 1;
-
-    arrayAll(array, result+rest, result);
-    // 1 = [1]
-    // 2 = [2] [1, 1]
-    // 3 = [2, 1] [1, 1, 1] [1, 2]
-    // 4 = [2, 2] [2, 1, 1] 
+    return ways;
 }
 
 int main(){
-    int array[] = {1, 2, 2, 1};
-    int arraySize = sizeof(array)/sizeof(array[0]);
-    //arrayAll(array, arraySize);
-    permute(array, 4, 2);
+    int n = stairs(1);
+    printf("%i\n", n);
 }
