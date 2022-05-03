3e269a980d9b944f443c642abd9e5cf76c43e9b6
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-05-03 11:07:51 -0300

    update

---
+++ Aula08/Exercício05.c
@@ -0,0 +1,19 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+void stairs(int stairsNumber){
+    int matrix[stairsNumber][stairsNumber];
+    for(int i=0; i <stairsNumber; i++){
+        int sum = 0;
+        for(int j=0; j <stairsNumber; j++){
+                     
+        }
+    }
+    // 1 = [1]
+    // 2 = [2] [1, 1]
+    // 3 = [2, 1] [1, 1, 1] [1, 2]
+    // 4 = [2, 2] [2, 1, 1]
+}
+
+int main(){
+}
