7a0dcb7a0d23209251f5eeb5cd23dba8b0822198
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-05-05 18:31:58 -0300

    update

--- Aula08/Exercício01.c
+++ Aula08/Exercício01.c
@@ -13,6 +13,16 @@ int twoSum(int *array, int sum, int arraySize){
     return 1;
 }
 
+// É possíveil fazer um algoritmo melhor utilizando: 
+// (1) Merge Sort para ordenação [O(n.log n)]
+// (2) Loop para percorrer o vetor verificando o número que deve ser somado ao elemento atual [O(n)]
+// (3) Pesquisa binário do termo a ser somado [O(log n)]
+
+// É possíveil fazer um algoritmo ainda melhor utilizando:
+// (1) Guardando os elementos em um dicionário onde a chave é o valor e o índice é a informação [O(n)]
+// (2) Looping para vericar se o valor k-v[i] está no dicionário [O(n)]
+// (3) Verificar se o valor k-v[i] está no dicioná [O(1)]
+
 int main(){
     int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9 , 10};
     int size = sizeof(array)/sizeof(array[0]);
