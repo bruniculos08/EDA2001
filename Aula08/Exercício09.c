e22b4a33f93bab4dd0c91a54ebb6a965964b8dee
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-05-09 10:52:18 -0300

    update

--- Aula08/Exercício09.c
+++ Aula08/Exercício09.c
@@ -1,47 +1,17 @@
 #include "stdlib.h"
 #include "stdio.h"
 
-typedef struct Node node;
-typedef struct Stack stack;
-struct Node{   
-    char *letter;
-    node *next;
-};
-struct Stack{
-    node *first;
-};
-
-char removeStack(stack *auxStack){
-    if(auxStack->first == NULL) return NULL;
-    node *removed = auxStack->first;
-    char r;
-    r = removed->letter;
-    auxStack->first = auxStack->first->next;
-    free(removed);    
-    return r;
-}
-
-void insertStack(stack *auxStack, char word[]){
-    int length = strlen(word);
-    node *newNode;
-    newNode = (node *)malloc(sizeof(node));
-    newNode->next = auxStack->first;
-    auxStack->first = newNode;
-    newNode->letter = (char *)malloc(sizeof(char)*length);
-    for(int i = 0; i < length; i++) newNode->letter[i] = word[i];
-    return;
-}
-
 void encode(char string[], int stringSize){
+    if(stringSize == 0) return;
+    int letter = string[0];
     int count = 1;
-    stack *auxStack;
-    auxStack->first = (node*)malloc(sizeof(node));
-    
-    for(int i=stringSize-1; i>=0; i--){
-        if(string[i] == auxStack->first->letter) count++;
+    for(int i = 1; i < stringSize; i++){
+        if(letter == string[i]) count++;
         else{
-            
-        }
+            printf("%i%c", count, letter);
+            letter = string[i];
+            count = 1;
+        }  
     }
 }
 
