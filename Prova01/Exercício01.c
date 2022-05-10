0e5e3ee820c2a40f44b9565249b7b96b3d955717
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-05-10 08:36:10 -0300

    update

---
+++ Prova01/Exercício01.c
@@ -0,0 +1,53 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+int countNumber(int *array, int arraySize, int number);
+int pertence(int *array, int arraySize, int number);
+
+//Obs.: meu código conta todas a possibilidades de pares
+
+int pairs(int *array, int arraySize){
+    int pares=0;
+    int tamanhoPares[arraySize];
+    int indexTamanhoPares = 0;
+
+    for(int i=0; i<arraySize; i++){
+        // 01. O vetor que "anota" os tamanhos já contados tem que ter seus elementos iguais a 0 inicialmente:
+        tamanhoPares[i] = 0;
+
+        // 02. Se o tamanho em questão já foi contabilizado pode-se pular para o próximo elemento
+        if(pertence(tamanhoPares, arraySize, array[i]) == 0) continue;
+        else{
+            // 03. Anotar tamanho no vetor de anotação e neste andar um elemento:
+            tamanhoPares[indexTamanhoPares] = array[i];
+            indexTamanhoPares++;
+
+            // 04. Calcular o número de pares:
+            pares = pares + (countNumber(array, arraySize, array[i])*(countNumber(array, arraySize, array[i])-1))/2;
+            
+        }
+    }
+    return pares;
+}
+
+int countNumber(int *array, int arraySize, int number){
+    int count = 0;
+    for(int i=0; i<arraySize; i++){
+        if(array[i] == number) count++;
+    }
+    return count;
+}
+
+int pertence(int *array, int arraySize, int number){
+    for(int i=0; i<arraySize; i++){
+        if(array[i]==0) return 1;
+        else if(array[i]==number) return 0;
+    }
+    return 1;
+}
+
+int main(){
+    int array[] = {6, 5, 2, 3, 5, 2, 2 , 1};
+    int arraySize = sizeof(array)/sizeof(array[0]);
+    printf("%i\n", pairs(array, arraySize));
+}
---
+++ Prova01/Exercício02.c
@@ -0,0 +1,9 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+int main(){
+    int array[] = {1,2,3,4,5};
+    int arraySize = sizeof(array)/sizeof(array[0]);
+    
+    
+}
