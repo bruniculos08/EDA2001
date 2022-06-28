56a96cee329e82e03df45b57db5b9ec60cbc898d
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-06-28 08:54:16 -0300

    update

--- Aula19/redBlackTree.h
+++ Aula19/redBlackTree.h
@@ -1,6 +1,12 @@
 enum coloring {red, black};
 typedef enum coloring coloring;
 
+typedef struct RedBlackTree tree;
+struct RedBlackTree {
+    node *root;
+    node *nullRoot;
+};
+
 typedef struct Node node;
 struct Node {
     node *father;
@@ -10,10 +16,13 @@ struct Node {
     int number;
 };
 
-node *addNode(node *root);
+node *addNode(tree *RB, node *root, int number);
 node *removeNode(node *root);
-node *createNode(int number);
+node *createNode(tree *RB, node *father, int number);
+tree *createTree();
 node *searchNode(node *root);
 node *rotateLeft(node *root);
 node *rotateRight(node *root);
 node *recolor(node *root);
+node *grandFather(node *root);
+node *uncle(node *root);
--- Aula19/redBlackTreeMain.c
+++ Aula19/redBlackTreeMain.c
@@ -1,12 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "redBlackTree.h"
 
-node *createNode(int number){
+node *createNode(tree *RB, node *father, int number){
     node *auxNode = (node*)malloc(sizeof(node));
     auxNode->number = number;
-    auxNode->father = NULL;
-    auxNode->left = NULL;
-    auxNode->right = NULL;
+    auxNode->father = father;
+    auxNode->left = RB->nullRoot;
+    auxNode->right = RB->nullRoot;
     auxNode->color = red;
 }
+
+node *addNode(tree *RB, node *root, int number){
+    node *auxNode = root;
+    while(true){
+        if(auxNode->number <= number && auxNode->right == NULL){ 
+            auxNode->right = createNode(RB, auxNode, number);
+            break;
+        }
+        else if(auxNode->number > number && auxNode->left == NULL){
+            auxNode->left = createNode(RB, auxNode, number);
+            break;
+        }
+        else if(auxNode->number <= number) auxNode = auxNode->right;
+        else auxNode = auxNode->left;
+    }
+    return root;
+}
+
+node *grandFather(node *root){
+    node *auxNode = root;
+    if(root->father != NULL){
+        return root->father
+    }
+}
+
+node *uncle(node *root);
+
